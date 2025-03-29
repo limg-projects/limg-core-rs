f3082bd86cf5d8b2a9e247d4013df42d799206c8
limg-projects <limg.github@gmail.com>
2025-03-29 20:54:30 +0900

    error系の修正

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,22 @@
+use ::core::fmt;
 
 pub type Result<T> = ::core::result::Result<T, Error>;
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum Error {
     ZeroImageDimensions,
     InputBufferTooSmall,
     OutputBufferTooSmall,
-    UnsupportFormat,
+    UnsupportedFormat,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::ZeroImageDimensions => write!(f, "Image width or height is zero"),
+            Error::InputBufferTooSmall => write!(f, "Input buffer is too small"),
+            Error::OutputBufferTooSmall => write!(f, "Output buffer is too small"),
+            Error::UnsupportedFormat => write!(f, "Unsupported image format or header"),
+        }
+    }
 }
