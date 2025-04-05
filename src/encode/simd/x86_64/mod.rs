0f24ae1b56882c4c3c870a819e023677bddf821e
limg-projects <limg.github@gmail.com>
2025-04-05 22:08:17 +0900

    simdのcfgを記述

--- src/encode/simd/x86_64/mod.rs
+++ src/encode/simd/x86_64/mod.rs
@@ -1,10 +1,12 @@
 
-mod sse41;
-mod avx2;
 
+#[cfg(target_feature = "avx2")]
+mod avx2;
 #[cfg(target_feature = "avx2")]
 pub use avx2::*;
 
+#[cfg(all(not(target_feature = "avx2"), target_feature = "sse4.1"))]
+mod sse41;
 #[cfg(all(not(target_feature = "avx2"), target_feature = "sse4.1"))]
 pub use sse41::*;
 
