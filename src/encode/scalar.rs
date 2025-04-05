95f47588d41038c1e95a75b7d0bd9fdb37bbd12d
limg-projects <limg.github@gmail.com>
2025-04-05 17:16:24 +0900

    encode/scalar.rsの修正

--- src/encode/scalar.rs
+++ src/encode/scalar.rs
@@ -1,17 +1,41 @@
-use crate::pixel::{rgb_to_pixel, PIXEL_BYTES};
-use ::core::ptr::{copy_nonoverlapping, read_unaligned, write_unaligned};
+use crate::pixel::{rgb_to_pixel, ColorType, PIXEL_BYTES};
+use ::core::ptr::copy_nonoverlapping;
+
+#[inline(always)]
+pub unsafe fn encode_from_rgb565_direct(data: &[u8], buf: &mut [u8], num_pixels: usize) {
+    unsafe { copy_nonoverlapping(data.as_ptr(), buf.as_mut_ptr(), num_pixels * PIXEL_BYTES); }
+}
+
+#[inline(always)]
+pub unsafe fn encode_from_rgb565_swap(data: &[u8], buf: &mut [u8], num_pixels: usize) {
+    let mut data_ptr = data.as_ptr().cast::<u16>();
+    let mut buf_ptr = buf.as_mut_ptr().cast::<u16>();
+
+    for _ in 0..num_pixels {
+        unsafe {
+            let pixel = data_ptr.read_unaligned();
+            buf_ptr.write_unaligned(pixel.swap_bytes());
+
+            data_ptr = data_ptr.add(1);
+            buf_ptr = buf_ptr.add(1);
+        }
+    }
+}
 
 macro_rules! encode_from_endian {
     ($endian: expr, $endian_fn: ident, $rgb888: ident, $rgb565: ident, $rgba8888: ident) => {
         #[inline(always)]
         pub unsafe fn $rgb888(data: &[u8], buf: &mut [u8], num_pixels: usize) {
-            let mut data_ptr = data.as_ptr().cast::<[u8; 3]>();
+            const COLOR_BYTES: usize = ColorType::bytes_per_pixel(ColorType::Rgb888);
+            
+            let mut data_ptr = data.as_ptr().cast::<[u8; COLOR_BYTES]>();
             let mut buf_ptr = buf.as_mut_ptr().cast::<u16>();
         
             for _ in 0..num_pixels {
                 unsafe {
-                    let pixel = rgb_to_pixel(*data_ptr).$endian_fn();
-                    write_unaligned(buf_ptr, pixel);
+                    let rgb = data_ptr.read();
+                    let pixel = rgb_to_pixel(rgb).$endian_fn();
+                    buf_ptr.write_unaligned(pixel);
         
                     data_ptr = data_ptr.add(1);
                     buf_ptr = buf_ptr.add(1);
@@ -22,33 +46,24 @@ macro_rules! encode_from_endian {
         #[inline(always)]
         pub unsafe fn $rgb565(data: &[u8], buf: &mut [u8], num_pixels: usize) {
             if cfg!(target_endian = $endian) {
-                unsafe { copy_nonoverlapping(data.as_ptr(), buf.as_mut_ptr(), num_pixels * PIXEL_BYTES); }
+                unsafe { encode_from_rgb565_direct(data, buf, num_pixels); }
             } else {
-                let mut data_ptr = data.as_ptr().cast::<u16>();
-                let mut buf_ptr = buf.as_mut_ptr().cast::<u16>();
-        
-                for _ in 0..num_pixels {
-                    unsafe {
-                        let pixel = read_unaligned(data_ptr);
-                        write_unaligned(buf_ptr, pixel.swap_bytes());
-        
-                        data_ptr = data_ptr.add(1);
-                        buf_ptr = buf_ptr.add(1);
-                    }
-                }
+                unsafe { encode_from_rgb565_swap(data, buf, num_pixels); }
             }
         }
 
         #[inline(always)]
         pub unsafe fn $rgba8888(data: &[u8], buf: &mut [u8], num_pixels: usize) {
-            let mut data_ptr = data.as_ptr().cast::<[u8; 4]>();
+            const COLOR_BYTES: usize = ColorType::bytes_per_pixel(ColorType::Rgba8888);
+
+            let mut data_ptr = data.as_ptr().cast::<[u8; COLOR_BYTES]>();
             let mut buf_ptr = buf.as_mut_ptr().cast::<u16>();
         
             for _ in 0..num_pixels {
                 unsafe {
-                    let rgba = *data_ptr;
+                    let rgba = data_ptr.read();
                     let pixel = rgb_to_pixel([rgba[0], rgba[1], rgba[2]]).$endian_fn();
-                    write_unaligned(buf_ptr, pixel);
+                    buf_ptr.write_unaligned(pixel);
         
                     data_ptr = data_ptr.add(1);
                     buf_ptr = buf_ptr.add(1);
